74b1cf99716eb7c040393f5d7196c418ba9663e3
Gregor Robertson <2076831r@student.gla.ac.uk>
2016-10-15 12:45:27 +0100

    user's can now attack

--- server-app/src/main/java/api/GameMap.java
+++ server-app/src/main/java/api/GameMap.java
@@ -1,10 +1,13 @@
 package api;
 
 
+import java.awt.Point;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
+import java.lang.Math;
 
 /* GameMap -> Deals with the Map of the Game
- * TODO write more here...
  */
 public class GameMap 
 {
@@ -13,10 +16,55 @@ public class GameMap
 
   public static final String[] SEEDS = {"plains"};
   
-  private MapElement[][] board; 
+  private MapElement[][] board;
+  private Map<Unit, Point> units;
   
   public GameMap() {
     board = generateNewMap();
+    units = new HashMap<Unit, Point>();
+  }
+  
+  public Point getUnitLocation(Unit unit) {
+    Point location;
+    try {
+      location = units.get(unit);
+    } catch (Exception e) {
+      location = null;
+    }
+    return location;
+  }
+  
+  public boolean addUnit(Unit unit, int x, int y) {
+    if (x < 0 || x > 49 || y < 0 || y > 49) {
+      return false;
+    }
+    Point location = new Point(x, y);
+    units.put(unit, location);
+    return true;
+  }
+  
+  public Unit unitAtPoint(int x, int y) {
+    Point p = new Point(x, y);
+    for (Unit unit : units.keySet()) {
+      if (units.get(unit).equals(p)) {
+        return unit;
+      }
+    }
+    return null;
+  }
+  
+  public boolean removeUnit(Unit unit) {
+    try {
+      units.remove(unit);
+    } catch(Exception e) {
+      return false;
+    }
+    return true;
+  }
+  
+  public static int distanceBetween(int aX, int aY, int bX, int bY) {
+    double distance = Math.hypot((aX-bX), (aY-bY));
+    return new Double(distance).intValue();
   }
   
   private MapElement[][] generateNewMap() {
--- server-app/src/main/java/api/PlayerStatus.java
+++ server-app/src/main/java/api/PlayerStatus.java
@@ -1,6 +1,7 @@
 package api;
 
 import java.util.Map;
+import java.awt.Point;
 import java.util.HashMap;
 
 /* PlayerStatus - Keeps state of a player in a given game
@@ -9,6 +10,7 @@ import java.util.HashMap;
 public class PlayerStatus
 {
   private final String iPlayerId;
+  private static final int STARTING_UNIT_COUNT = 5;
   
   private final GameMap iMap;
   private final Map<String, Unit> iUnits;
@@ -23,7 +25,23 @@ public class PlayerStatus
   
   public synchronized boolean attack(String aUnit, int aX, int aY)
   {
-    return false;
+    Unit unit = iUnits.get(aUnit);
+    if (unit == null) {
+      return false;
+    }
+    synchronized(iMap) {
+      Point p = iMap.getUnitLocation(unit);
+      int x = new Double(p.getX()).intValue();
+      int y = new Double(p.getY()).intValue();
+      Unit enemy = iMap.unitAtPoint(aX, aY);
+      if (unit.getRange() >= GameMap.distanceBetween(aX, aY, x, y)) {
+        boolean dead = unit.attack(enemy);
+        if (dead) {
+          iMap.removeUnit(enemy);
+        }
+      }
+    }
+    return true;
   }
   
   public synchronized boolean move(String aUnit, int aX, int aY)
@@ -45,7 +63,13 @@ public class PlayerStatus
   
   private Map<String, Unit> getNewUnits()
   {
-    return new HashMap<String, Unit>();  
+    HashMap<String, Unit> newUnits = new HashMap<String, Unit>();
+    Unit[] units = Unit.generateUnits(STARTING_UNIT_COUNT);
+    for (int i = 0; i < STARTING_UNIT_COUNT; i++) {
+      String unitId = iPlayerId + "_" + String.valueOf(i);
+      newUnits.put(unitId, units[i]);
+    }
+    return newUnits;  
   }
   
   
