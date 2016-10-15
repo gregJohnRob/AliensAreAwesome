4e170e1c7c36a3e3c838489961e33a196b2d877a
projectgoav <edm22@hotmail.co.uk>
2016-10-15 17:10:50 +0100

    Implemented game/leave.

--- server-app/src/main/java/api/endpoints/GameEndpoint.java
+++ server-app/src/main/java/api/endpoints/GameEndpoint.java
@@ -7,11 +7,8 @@ import com.google.gson.Gson;
 
 import api.Game;
 import api.Message;
-import api.responses.GameCreateResponse;
-import api.responses.GameListResponse;
-import api.responses.PlayerGameResponse;
-import services.storage.GameRepository;
-import services.storage.PlayerRepository;
+import api.responses.*;
+import services.storage.*;
 
 
 public class GameEndpoint
@@ -47,9 +44,28 @@ public class GameEndpoint
     return GameCreateResponse.Success(g);
   }
 
-  private Object doGameLeave(Request req, Response res) {
-    // TODO Auto-generated method stub
-    return null;
+  private Message<Boolean> doGameLeave(Request req, Response res)
+  {
+    QueryParamsMap query = req.queryMap();
+    
+    String userId = query.get("clientId").value();
+    String gameId = query.get("gameId").value();
+    
+    if(userId == null || userId == "")              { return GameLeaveResponse.NoClientId; }  
+    if(!PlayerRepository.instance.contains(userId)) { return GameLeaveResponse.InvalidClientId; }
+    
+    if(gameId == null || gameId == "")            { return GameLeaveResponse.NoGameId; }  
+    if(!GameRepository.instance.contains(gameId)) { return GameLeaveResponse.InvalidGameId; }
+   
+    Game g = GameRepository.instance.getById(gameId);
+
+    if(g.hasPlayer(userId))
+    {
+      g.removePlayer(userId);
+      return GameLeaveResponse.Success;
+    }
+    
+    return GameLeaveResponse.PlayerNotInGame;
   }
 
   private Object doGameJoin(Request req, Response res) {
---
+++ server-app/src/main/java/api/responses/GameLeaveResponse.java
@@ -0,0 +1,28 @@
+package api.responses;
+
+import api.Message;
+
+public class GameLeaveResponse 
+{
+  public static Message<Boolean> NoClientId         = sendMsg("No param 'clientId' given.", false);
+  public static Message<Boolean> InvalidClientId    = sendMsg("The given clientId doesn't exist.", false);
+  
+  public static Message<Boolean> NoGameId           = sendMsg("No param 'gameId' given.", false);
+  public static Message<Boolean> InvalidGameId      = sendMsg("The given gameId doesn't exist.", false);
+  
+  public static Message<Boolean> PlayerNotInGame    = sendMsg("The given clientId was not present in the game.", false);
+  
+  public static Message<Boolean> Success            = sendMsg("Ok", true);
+  
+  
+  public static Message<Boolean> sendMsg(String aMessage, boolean aResult)
+  {
+    Message<Boolean> ms = new Message<Boolean>();
+    
+    ms.Message = aMessage;
+    ms.Data = aResult;
+    
+    return ms;
+  }
+  
+}
