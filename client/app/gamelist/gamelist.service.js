66d8cc57c9575dd2d03b6b725d1ab2c002711af8
ExogenesisBen <ben.d.jackson@outlook.com>
2016-10-15 15:41:01 +0100

    Responce -> Response

--- client/app/gamelist/gamelist.service.js
+++ client/app/gamelist/gamelist.service.js
@@ -12,20 +12,20 @@
     return service;
 
     function mygames(id){
-      return $resource('http://localhost:4567/player/game/').get({clientId:'d703932b-c9d1-45a7-bd5f-d86a18fb24c6'} , function(responce){
-        return responce;
+      return $resource('http://localhost:4567/player/game/').get({clientId:'d703932b-c9d1-45a7-bd5f-d86a18fb24c6'} , function(response){
+        return response;
       })
     }
 
     function allgames(){
-      return $resource('http://localhost:4567/game/list').get({},function(responce){
-        return responce.Data.Entries;
+      return $resource('http://localhost:4567/game/list').get({},function(response){
+        return response.Data.Entries;
       })
     }
 
     function create(){
-      return $resource('http://localhost:4567/game/create').get(function(responce){
-        return responce.Data.Id;
+      return $resource('http://localhost:4567/game/create').get(function(response){
+        return response.Data.Id;
       });
     }
   }
